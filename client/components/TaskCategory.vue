86ba4c86695e8b9bc3fd35c500c9e7c7e381af73
Onesinus SPT <onesinus231@gmail.com>
2020-02-13 15:09:42 +0700

    Sudah mengimplementasikan ke App.vue

---
+++ client/components/TaskCategory.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Coba {{ coba }}</h1>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                title: "Ganteng"
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
--- client/js/main.js
+++ client/js/main.js
@@ -1,90 +1,15 @@
+import Vue from "vue";
+import App from '../components/App.vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+
 let kanban = new Vue({
-    el: ".kanban",
-    data: {
-        "backend_url": "http://localhost:3000",
-        "taskCategory": null,
-        "input_category_id": "",
-        "input_title": "",
-        "input_description": ""
-    },
-    methods: {
-        getTasks: function(){
-            fetch(`${this.backend_url}/projects`)
-            .then(res => {
-                return res.json();  
-            })
-            .then(data => {
-                this.taskCategory = data
-            })
-            .catch(err => console.log(err));
-        },
-        addTask: function(idxCategory) {
-            let objAdd = {
-                title: this.input_title,
-                description: this.input_description,
-                UserId: 1,
-                ProjectId: idxCategory
-            };
-
-            fetch(`${this.backend_url}/tasks`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(objAdd)
-            })  
-            .then(res => {
-                console.log(res);
-                this.getTasks();
-            })
-            .catch(err => {
-                console.log(err);
-            })            
-        },
-        updateTask: function(idxCategory) {
-            let objEdit = {
-                title: this.input_title,
-                description: this.input_description,
-                UserId: 1,
-                ProjectId: idxCategory
-            };
-
-            fetch(`${this.backend_url}/tasks/${this.input_id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(objEdit)
-            })  
-            .then(res => {
-                console.log(res);
-                this.getTasks();
-            })
-            .catch(err => {
-                console.log(err);
-            })
-        },
-        resetModal: function() {
-            this.input_category_id  = null;
-            this.input_id           = null;
-            this.input_title        = "";
-            this.input_description  = "";
-        },
-        setModalValue: function(task){
-            this.input_category_id  = task.categoryId;
-            this.input_id           = task.id;
-            this.input_title        = task.title;
-            this.input_description  = task.description;
-        },
-        handleOk: function() {
-            if (this.input_id == undefined) {
-                this.addTask(this.input_category_id);
-            }else{
-                this.updateTask(this.input_category_id);
-            }
-        }
-    },
-    beforeMount() {
-        this.getTasks();
-    }
+    el: ".app",
+    render: h => h(App)
 });
