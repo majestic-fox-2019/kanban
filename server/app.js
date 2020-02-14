3815a2e93c6613b1870548f5bef701ff99ba53e6
ilhamabdulm <a.marzmellow21@gmail.com>
2020-02-14 14:16:37 +0700

    ganti app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,3 @@
-if (
-  process.env.NODE_ENV == 'development' ||
-  process.env.NODE_ENV == 'production'
-) {
-  require('dotenv').config()
-}
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
