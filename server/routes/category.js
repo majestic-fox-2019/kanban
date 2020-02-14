d361609c4f56c26b522588e9dab786f018f6b9e6
Mazda94 <m.alli9401@gmail.com>
2020-02-15 00:16:21 +0700

    update readme and add authentifikasi on project

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const route = express.Router()
 const controller = require('../controllers/category')
+const authToken = require('../middleware/authToken')
+const authorization = require('../middleware/authorization')
 
+route.use(authToken)
 route
     .get('/:id', controller.getCategory)
     .post('/', controller.postCategory)
