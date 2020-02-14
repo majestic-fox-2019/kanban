28c446317b5d3954ab59668f4a43888753667880
jetly sandita <jetly27sandita@gmail.com>
2020-02-14 20:14:22 +0700

    delete firebase config value

--- client/apis/firebase.js
+++ client/apis/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 import 'firebase/auth'
 const firebaseConfig = {
-  apiKey: "AIzaSyBXCFDhLaDlL50gn-zGDnVWq58mRKA1zdk",
-  authDomain: "kanban-116cb.firebaseapp.com",
-  databaseURL: "https://kanban-116cb.firebaseio.com",
-  projectId: "kanban-116cb",
-  storageBucket: "kanban-116cb.appspot.com",
-  messagingSenderId: "512116520515",
-  appId: "1:512116520515:web:0e19c771c4acb942fe06f6"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(firebaseConfig)
